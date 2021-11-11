86046f0f4dea842138bb2e6840c43c73fbf7bec2
Victor Shin <victor.kamiguchi@gmail.com>
2021-11-11 19:27:17 -0300

    C4 R4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -29,6 +30,15 @@ export default class Header extends Component {
     return ((load) ? <Loading />
       : (
         <section data-testid="header-component">
+          <Link to="/search" data-testid="link-to-search">
+            Buscar
+          </Link>
+          <Link to="/favorites" data-testid="link-to-favorites">
+            Favoritos
+          </Link>
+          <Link to="/profile" data-testid="link-to-profile">
+            Perfil
+          </Link>
           <h2>Cabeçalho</h2>
           <h2 data-testid="header-user-name">{loginName}</h2>
         </section>
