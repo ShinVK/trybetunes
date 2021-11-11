f391c5ee424fe3aef43fcb865172845f63deb984
Victor Shin <victor.kamiguchi@gmail.com>
2021-11-11 16:08:40 -0300

    C1 Requirement 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route path="/search" exact component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" exact component={ Profile } />
+          <Route path="/profile/edit" exact component={ ProfileEdit } />
+          <Route path="/" exact component={ Login } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>);
   }
 }
 
