2f647f83e05896d72a0f97bfbb0736274ceb3d03
shinvk <victor.kamiguchi@gmail.com>
2021-11-17 09:28:25 -0300

    C13 R13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      load: false,
+      dataUsr: [],
+    };
+    this.fetchUser = this.fetchUser.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+  async fetchUser() {
+    this.setState({ load: true });
+    await getUser().then((dataU) => this.setState({
+      dataUsr: dataU,
+    }));
+    this.setState({ load: false });
+  }
+
   render() {
+    const { load, dataUsr } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-profile">
-          <h2>Profile page</h2>
+          {(load)
+            ? <Loading />
+            : (
+              <div className="cardProfile">
+                <img
+                  src={ dataUsr.image }
+                  alt="Autorretrato do usuário"
+                  data-testid="profile-image"
+                />
+                <h2>Nome</h2>
+                <p>{ dataUsr.name }</p>
+                <h2>Email</h2>
+                <p>{ dataUsr.email }</p>
+                <h2>Descrição</h2>
+                <p>{ dataUsr.description }</p>
+                <Link to="/profile/edit">Editar perfil</Link>
+              </div>
+            )}
         </div>
       </>
     );
--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,13 +1,94 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 export default class ProfileEdit extends Component {
+  constructor() {
+    super();
+    this.state = {
+      load: false,
+      usrData: [],
+    };
+    this.fetchUsr = this.fetchUsr.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchUsr();
+  }
+
+  async fetchUsr() {
+    this.setState({
+      load: true,
+    });
+    await getUser().then((usr) => this.setState({
+      usrData: usr,
+    }));
+    this.setState({
+      load: false,
+    });
+  }
+
+  // isDisabled() {
+  //   const { usrData } = this.state;
+  // }
+
   render() {
+    const { usrData, load } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-profile-edit">
-          <h2>Profile Edit Page</h2>
+          {(load) ? <Loading />
+            : (
+              <div className="form__profile">
+                <h2>Profile Edit Page</h2>
+                <form>
+                  <label htmlFor="nameInput">
+                    Nome
+                    <input
+                      id="nameInput"
+                      type="text"
+                      value={ usrData.name }
+                      data-testid="edit-input-name"
+                    />
+                  </label>
+                  <label htmlFor="emailInput">
+                    Email
+                    <input
+                      id="emailInput"
+                      type="email"
+                      value={ usrData.email }
+                      data-testid="edit-input-email"
+                    />
+                  </label>
+                  <label htmlFor="descriptionInput">
+                    Descrição
+                    <textarea
+                      id="descriptionInput"
+                      type="textarea"
+                      value={ usrData.description }
+                      data-testid="edit-input-description"
+                    />
+                  </label>
+                  <label htmlFor="imageInput">
+                    Imagem
+                    <input
+                      id="imageInput"
+                      type="text"
+                      value={ usrData.image }
+                      data-testid="edit-input-image"
+                    />
+                  </label>
+                  <button
+                    type="submit"
+                    data-testid="edit-button-save"
+                    disabled={ this.isDisabled() }
+                  >
+                    Salvar
+                  </button>
+                </form>
+              </div>)}
         </div>
       </>
     );
