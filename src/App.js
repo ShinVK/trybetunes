b759124ddb9f0fdb0792eaea46f3f96b363acdb9
shinvk <victor.kamiguchi@gmail.com>
2022-04-08 00:52:20 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
+import { ThemeProvider } from '@mui/material';
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-import Login from './pages/Login';
-import Album from './pages/Album';
-import Favorites from './pages/Favorites';
-import NotFound from './pages/NotFound';
-import Profile from './pages/Profile';
-import ProfileEdit from './pages/ProfileEdit';
-import Search from './pages/Search';
+import Routes from './routes/Routes';
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <p>TrybeTunes</p>
-        <Switch>
-          <Route path="/search" exact component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" exact component={ Profile } />
-          <Route path="/profile/edit" exact component={ ProfileEdit } />
-          <Route path="/" exact component={ Login } />
-          <Route path="*" component={ NotFound } />
-        </Switch>
-      </BrowserRouter>);
+      <ThemeProvider>
+        <Routes />
+      </ThemeProvider>
+    );
   }
 }
 
